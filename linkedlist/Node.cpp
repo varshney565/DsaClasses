25bdf9539ff573ab3afa93622f1056db7c175c64
varshey565 <uietcse1923shivamvarshney@kuk.ac.in>
2024-10-12 23:07:03 +0530

    class2

--- linkedlist/Node.cpp
+++ linkedlist/Node.cpp
@@ -5,6 +5,7 @@ class Node {
 
     Node(int data) {
         this->data = data;
+        this->next = nullptr;
     }
 
     ~Node() {
--- linkedlist/linkedlist.cpp
+++ linkedlist/linkedlist.cpp
@@ -12,7 +12,7 @@ void print(Node* head) {
      */
 
     while(curr != NULL) {
-        cout<<curr->data<<"->";
+        cout<<curr->data<<" ";
         curr = curr->next;
     }
     // here i need the head of the linked list
@@ -30,11 +30,73 @@ int length(Node* head) {
 
 Node* addAtEnd(int data,Node* head) {
     //TODO
+    if(head == NULL) {
+        Node* temp = new Node(data);
+        return temp;
+    }
+    Node* curr = head;
+    while(curr->next != nullptr) {
+        curr = curr->next;
+    }
+    Node* temp = new Node(data);
+    curr->next = temp;
+    return head;
 }
 
 
-Node* addAtKthNode(int data,Node* head,int k) {
+Node* addAtKthPos(int data,Node* head,int k) {
     //TODO
+    if(k < 0) {
+        return head;
+    }
+
+    if(k == 0) {
+        Node* curr = new Node(data);
+        curr->next = head;
+        return curr;
+    }
+
+    Node* curr = head;
+    int position = 0;
+    while(curr != nullptr && position < k-1) {
+        position++;
+        curr = curr->next;
+    }
+
+    Node* temp = new Node(data);
+    if(curr != nullptr) {
+        temp->next = curr->next;
+        curr->next = temp;
+    }
+    return head;
+}
+
+
+Node* removeFromHead(Node* head) {
+    Node* curr = head;
+    curr = curr->next;
+    head->next = nullptr;
+    delete head;
+    return curr;
+}
+
+Node* removeFromTail(Node* head) {
+    //TODO
+}
+
+Node* removeFromKthPos(Node* head,int k) {
+    //TODO
+}
+
+
+Node* findMid(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+    while(/**check**/ fast != nullptr && fast->next != nullptr) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
 }
 
 Node* addAtBeginning(int data,Node* head) {
@@ -48,18 +110,30 @@ int main() {
     Node *second = new Node(3);
     Node* third = new Node(5);
     Node* fourth = new Node(6);
+    Node* fifth = new Node(7);
 
     head->next = second;
     second->next = third;
     third->next = fourth;
-    fourth->next = NULL;
+    fourth->next = fifth;
 
-    print(head);
-    int l = length(head);
-    cout<<"\nLength of the linked list : "<<l<<"\n";
+    // print(head);
+    // int l = length(head);
+    // cout<<"\nLength of the linked list : "<<l<<"\n";
 
-    head = addAtBeginning(-1,head);
+    // head = addAtBeginning(-1,head);
+    // print(head);
+    // cout<<"\n";
+    // head = addAtEnd(8,head);
+    // print(head);
+    // cout<<"\n";
+    // head = addAtKthPos(100,head,4);
+    // head = removeFromHead(head);
     print(head);
-    int l2 = length(head);
-    cout<<"\nLength of the linked list : "<<l2<<"\n";
+
+    cout<<"\n";
+    Node* midNode = findMid(head);
+    cout<<"MidNode Data : "<<midNode->data<<"\n";
+    // int l2 = length(head);
+    // cout<<"\nLength of the linked list : "<<l2<<"\n";
 }
